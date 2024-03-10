81b24a310d722ca7d06b67c698c6a74674ab3a9f
Plopmenz <plopmenz@gmail.com>
2024-03-10 22:40:06 +0100

    deployment update

--- deploy/deploy.ts
+++ deploy/deploy.ts
@@ -4,15 +4,19 @@ import {
   deploy as tasksDeploy,
 } from "../lib/openrd-foundry/deploy/deploy";
 import {
-  TaskDisputesDeploymentSettings,
+  DeployTaskDisputesSettings,
   deployTaskDisputes,
-} from "./TaskDisputes";
-import { TaskDraftsDeploymentSettings, deployTaskDrafts } from "./TaskDrafts";
+} from "./extensions/TaskDisputes";
+import {
+  DeployTaskDraftsSettings,
+  deployTaskDrafts,
+} from "./extensions/TaskDrafts";
 
 export interface DeploymentSettings {
   tasksDeployment: TasksDeployment;
-  taskDisputeDeploymentSettings: Omit<TaskDisputesDeploymentSettings, "tasks">;
-  taskDraftsDeploymentSettings: Omit<TaskDraftsDeploymentSettings, "tasks">;
+  taskDisputeDeploymentSettings: Omit<DeployTaskDisputesSettings, "tasks">;
+  taskDraftsDeploymentSettings: Omit<DeployTaskDraftsSettings, "tasks">;
+  forceRedeploy?: boolean;
 }
 
 export interface Deployment {
@@ -24,23 +28,32 @@ export async function deploy(
   deployer: Deployer,
   settings?: DeploymentSettings
 ): Promise<Deployment> {
+  if (settings?.forceRedeploy !== undefined && !settings.forceRedeploy) {
+    return await deployer.loadDeployment({ deploymentName: "latest.json" });
+  }
+
   deployer.startContext("lib/openrd-foundry");
   const tasksDeployment =
     settings?.tasksDeployment ?? (await tasksDeploy(deployer));
   deployer.finishContext();
 
   const taskDisputes = await deployTaskDisputes(deployer, {
-    ...(settings?.taskDisputeDeploymentSettings ?? {}),
     tasks: tasksDeployment.tasks,
+    ...(settings?.taskDisputeDeploymentSettings ?? {}),
   });
 
   const taskDrafts = await deployTaskDrafts(deployer, {
-    ...(settings?.taskDraftsDeploymentSettings ?? {}),
     tasks: tasksDeployment.tasks,
+    ...(settings?.taskDraftsDeploymentSettings ?? {}),
   });
 
-  return {
+  const deployment = {
     taskDisputes: taskDisputes,
     taskDrafts: taskDrafts,
   };
+  await deployer.saveDeployment({
+    deploymentName: "latest.json",
+    deployment: deployment,
+  });
+  return deployment;
 }
